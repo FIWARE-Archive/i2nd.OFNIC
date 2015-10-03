01158e15ac796943ea595523eab1a181c443d6c8
Federico Cimorelli <cimorellifederico@gmail.com>
2015-10-03 19:00:42 +0200

    Minor

--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-if[ ! -f /opt/ofnic/ofnic-uniroma1-LATEST.jar ]
-then
-    echo "============Initialising Data============"
-    cp ofnic-uniroma1-LATEST.jar /opt/ofnic/ofnic-uniroma1-LATEST.jar
-    cp -r opendaylight /opt/ofnic/
-    echo "Copied OpenDaylight to /opt/ofnic"
-    echo "Copied ofnic-uniroma1-LATEST.jar to /opt/ofnic"
-fi
 cd /opt/ofnic/
 echo "============STARTING OFNIC============="
 sh ./opendaylight/run.sh &
