5463874125170d1f721b208ccad91b3aa3c15390
Federico Cimorelli <cimorellifederico@gmail.com>
2016-03-11 12:20:00 +0100

    -

--- docs/conf.py
+++ docs/conf.py
@@ -110,7 +110,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'alabaster'
-html_style = './fiware_readthedocs.css'
+html_style = 'css/fiware_readthedocs.css'
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
@@ -141,7 +141,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['custom']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
