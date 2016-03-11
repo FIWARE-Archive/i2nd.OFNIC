bb453c8141e5c0517c3f735c219e0417bc3de013
Federico Cimorelli <cimorellifederico@gmail.com>
2016-03-11 12:32:14 +0100

    -

--- docs/docs/conf.py
+++ docs/docs/conf.py
@@ -138,6 +138,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_style = 'fiware_readthedocs.css'
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
