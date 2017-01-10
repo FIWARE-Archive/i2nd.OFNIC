157f891ce4ed6cf519fd8b403582ef0ded937d27
FIWARE-UniRoma1 <cimorelli@dis.uniroma1.it>
2017-01-10 15:00:10 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "============STARTING OFNIC============="
-sh /opt/ofnic/opendaylight/run.sh &
+./opendaylight/run.sh &
 echo "Waiting Opendaylight starts..."
 sleep 120
 java -jar ./ofnic-uniroma1-LATEST.jar
